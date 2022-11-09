9d8e43edf0fdcfa7dffdfcb0f62093e7d6dc1cb5
RiDanilova <ri.charminglady@gmail.com>
2022-11-09 14:30:44 +0300

    Initial commit

---
+++ avito/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from ads.views import root
+from avito import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", root),
+    path("cat/", include("ads.urls.category_urls")),
+    path("ad/", include("ads.urls.ad_urls")),
+    path("user/", include("users.urls")),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
